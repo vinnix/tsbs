272301adf3f2d043cb661c02fb84d2f0cb994ddf
Vinícius Schmidt <vinics@amazon.com>
2021-03-01 15:07:38 +0000

    Making field_index_count variable and keeping default of 1. Adding Interval as parameter for handling postgres native partitions

--- scripts/load/load_timescaledb.sh
+++ scripts/load/load_timescaledb.sh
@@ -13,7 +13,7 @@ DATABASE_USER=${DATABASE_USER:-postgres}
 
 # Load parameters - personal
 CHUNK_TIME=${CHUNK_TIME:-8h}
-PARTITIONS=${PARTITIONS:-0}
+PARTITIONS=${PARTITIONS:-0} 
 HASH_WORKERS=${HASH_WORKERS:-false}
 TIME_PARTITION_INDEX=${TIME_PARTITION_INDEX:-false}
 PERF_OUTPUT=${PERF_OUTPUT:-}
@@ -22,12 +22,23 @@ IN_TABLE_PARTITION_TAG=${IN_TABLE_PARTITION_TAG:-true}
 USE_HYPERTABLE=${USE_HYPERTABLE:-true}
 DO_CREATE_DB=${DO_CREATE_DB:-true}
 FORCE_TEXT_FORMAT=${FORCE_TEXT_FORMAT:-false}
-NATIVE_PART=${NATIVE_PART:-false}
-TS_START=${TS_START:-"2016-01-01T00:00:00Z"}
-TS_END=${TS_END:-"2016-01-04T00:00:00Z"}
+FIELD_INDEX_COUNT=${FIELD_INDEX_COUNT:-1}
 
 EXE_DIR=${EXE_DIR:-$(dirname $0)}
 source ${EXE_DIR}/load_common.sh
+
+# Load parameters - for native postgresql partitions
+NATIVE_PART=${NATIVE_PART:-false}
+TS_START=${TS_START:-"2021-01-01T00:00:00Z"}
+TS_END=${TS_END:-"2021-01-04T00:00:00Z"}
+NATIVE_PART_INTERVAL=${NATIVE_PART_INTERVAL:-"4hours"}
+NATIVE_AUX_STR=""
+
+if [ "${NATIVE_PART}" = true ];
+then
+    NATIVE_AUX_STR=" --native-partitions=${NATIVE_PART} --time-start="${TS_START}" --time-end="${TS_END}" --native-partitions-interval="${NATIVE_PART_INTERVAL}" "
+fi
+
 
 while ! pg_isready -h ${DATABASE_HOST}; do
     echo "Waiting for timescaledb"
@@ -47,12 +58,9 @@ cat ${DATA_FILE} | gunzip | $EXE_FILE_NAME \
                                 --in-table-partition-tag=${IN_TABLE_PARTITION_TAG} \
                                 --hash-workers=${HASH_WORKERS} \
                                 --time-partition-index=${TIME_PARTITION_INDEX} \
-                                --native-partitions=${NATIVE_PART} \
                                 --partitions=${PARTITIONS} \
                                 --chunk-time=${CHUNK_TIME} \
                                 --write-profile=${PERF_OUTPUT} \
-                                --field-index-count=1 \
+                                --field-index-count=${FIELD_INDEX_COUNT} \
                                 --do-create-db=${DO_CREATE_DB} \
-                                --force-text-format=${FORCE_TEXT_FORMAT} \
-                                --time-start="${TS_START}" \
-                                --time-end="${TS_END}" 
+                                --force-text-format=${FORCE_TEXT_FORMAT} ${NATIVE_AUX_STR}
